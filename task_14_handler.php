8ccf18db3f1bf9a516547f6f75952a0472492ffa
igotis <igotiss@gmail.com>
2021-12-15 19:17:01 +0200

    task_14 Done

---
+++ task_14_handler.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+
+$input_email = $_POST['email'];
+$input_password = $_POST['password'];
+function passwordHash($password) {
+    return hash( 'sha256', $password);
+}
+
+
+
+$pdo = new PDO('mysql:host=localhost; dbname=php_learn', 'root', 'root');
+$sql = 'SELECT * FROM users WHERE email=:email';
+$statement = $pdo->prepare($sql);
+$statement->execute(['email' => $input_email]);
+$task = $statement->fetch(PDO::FETCH_ASSOC);
+
+
+if(empty($task)) {
+    $message = "Такой ел адрес не зарегистрирован";
+    $_SESSION['danger'] = $message;
+
+    header("Location: /task_14.php");
+    exit;
+}
+
+$dbPassword = $task['password'];
+
+function passwordVerify($input_password, $dbPassword) {
+    return hash_equals(passwordHash($input_password),  $dbPassword);
+}
+ if (passwordVerify($input_password, $dbPassword)) {
+     $message = "Логин и пароль верные, вы авторизованы";
+     $_SESSION['success'] = $message;
+     $_SESSION['login'] = $input_email;
+
+     header("Location: /task_14.php");
+     exit;
+ } else {
+     $message = "пароль введен неверно, повторите ввод";
+     $_SESSION['danger'] = $message;
+
+     header("Location: /task_14.php");
+     exit;
+ }
+
+var_dump();
